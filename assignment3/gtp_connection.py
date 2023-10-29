6ba33ebf6ef36ff67ba1df32e1c3b00e92916814
ravdoc34 <101607709+ravdoc34@users.noreply.github.com>
2023-10-29 15:37:44 -0600

    Update gtp_connection.py

--- assignment3/gtp_connection.py
+++ assignment3/gtp_connection.py
@@ -363,10 +363,9 @@ class GtpConnection:
     def policy_policytype_cmd(self,args: List[str]):
 
         if input == 'random' or 'rule_based':
-            #self.respond("only 2 options: random or rule_based")
             self.policytype = args[0]
             print(self.policytype)
-        else:
+        elif input != 'random' or 'rule_based':
             self.respond("nope")
         
 
