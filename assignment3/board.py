dfceed1b0ed25324e8bab2b8689fb29b10bd57f8
ravdoc34 <101607709+ravdoc34@users.noreply.github.com>
2023-11-05 21:33:34 -0700

    capture, check pattern

--- assignment3/board.py
+++ assignment3/board.py
@@ -445,6 +445,27 @@ class GoBoard(object):
     #         self.board[move] = EMPTY #resets the board 
     #     return captured_moves
 
+    
+    def check_pattern(self,  point: GO_POINT, color: GO_COLOR) -> bool:
+        if self.board[point] != EMPTY:
+            return False
+        self.board[point] = color
+        self.current_player = opponent(color)
+        self.last2_move = self.last_move
+        self.last_move = point
+        O = opponent(color)
+        offsets = [1, -1, self.NS, -self.NS, self.NS+1, -(self.NS+1), self.NS-1, -self.NS+1]
+        for offset in offsets:
+            if self.board[point+offset] == O and self.board[point+(offset*2)] == O and self.board[point+(offset*3)] == color:
+                self.board[point+offset] = EMPTY
+                self.board[point+(offset*2)] = EMPTY
+                if color == BLACK:
+                    self.black_captures += 2
+                else:
+                    self.white_captures += 2
+        return True
+
+
     def Capture(self):
         captured_moves = []
         legal_moves = self.get_empty_points()
@@ -454,16 +475,14 @@ class GoBoard(object):
             self.play_move(move, player_color)
             color = self.detect_two_in_a_row()
             if color == player_color:
-                captured_moves.append(move)
+            #     captured_moves.append(move)
             
             if player_color == WHITE and self.white_captures >= 2:
-            #     capture_moves = self.detect_two_in_a_row()
-            #     if move in capture_moves:
-                captured_moves.append(move)
+                if self.check_pattern(point, color):
+                    captured_moves.append(move)
             if player_color == BLACK and self.black_captures >= 2:
-            #     capture_moves = self.detect_two_in_a_row()
-            #     if move in capture_moves:
-                captured_moves.append(move)
+                if self.check_pattern(point, color):
+                    captured_moves.append(move)
            
 
             self.board[move] = EMPTY
