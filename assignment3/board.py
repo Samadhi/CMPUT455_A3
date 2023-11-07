9c16a8b1d01bc4223764fa611bcb58d29b65096f
ravdoc34 <101607709+ravdoc34@users.noreply.github.com>
2023-11-06 17:57:00 -0700

    capture

--- assignment3/board.py
+++ assignment3/board.py
@@ -437,10 +437,10 @@ class GoBoard(object):
             
             if player_color == WHITE and self.white_captures >= 2:
                 captured_moves.append(move)
-                self.white_captures -= 0
+                self.white_captures -= 3
             if player_color == BLACK and self.black_captures >= 2:
                 captured_moves.append(move)
-                self.black_captures -= 0
+                self.black_captures -= 3
            
 
             self.board[move] = EMPTY
